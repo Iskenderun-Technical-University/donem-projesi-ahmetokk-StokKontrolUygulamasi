4534e08f8fe9df68d83506807bb7277b98e95517
Ahmet Ok <ahmetok2003@gmail.com>
2023-05-06 23:47:21 +0300

    güncelleme

---
+++ stokkontrol/stokkontrol/Form1.cs
@@ -0,0 +1,15 @@
+namespace stokkontrol
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
